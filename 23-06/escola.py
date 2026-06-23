366e58e3857a7e3d844889adaa20ec8e9ba24c85
kkaique69 <kaique.s.reis2009@gmail.com>
2026-06-23 14:42:33 -0300

    Add files via upload

---
+++ 23-06/escola.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+class Pessoa:
+    def __init__(self, CPF:str, nome:str) -> None:
+        self.CPF = CPF;
+        self.nome = nome;
+        self.listaFrequencia = [];
+        
+    def addPresenca(self, ID:int, data:date) -> None:
+        self;listaPresenca.append(Presenca(ID, data));
+
+class Docente(Pessoa):
+    def __init__(self, CPF:str, nome:str, salario:float) -> None:
+        super().__init__(CPF, nome);
+        self.salario = salario;
+
+class Estudante(Pessoa):
+    def __init__(self, CPF:str, nome:str, RA:str) -> None:
+        super().__init__(CPF, nome);
+        self.RA = RA;
+        listaAtividade = [];
+
+    def addAtividade(self, atv:Atividade) -> None:
+        self.listaAtividade.append(atv);
+
+class Presenca:
+    def __init__(self, ID:int, data:str) -> None:
+        self.ID = ID;
+        self.data = data;
+        
+class Atividade:
+    def __init__(self, codigo:int, descricao:str, valor:float) -> None:
+        self.codigo = codigo;
+        self.descricao = descricao;
+        self.valor = valor;
